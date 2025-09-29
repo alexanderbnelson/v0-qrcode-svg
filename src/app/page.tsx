b4af64a4c5c069980bf86333b269ffeda4ff25f9
Alex Nelson <alex@postprojects.com>
2025-09-29 11:25:16 -0700

    chore: update packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { QrCodeGenerator } from "@/components/qr-code-generator";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <QrCodeGenerator />
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
+      <Suspense fallback={<div>Loading...</div>}>
+        <QrCodeGenerator />
+      </Suspense>
     </div>
   );
 }
